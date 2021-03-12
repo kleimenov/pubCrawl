b7bb89bbee37a19e2d0228e70769c4bfdee942cf
Иван <kleimenov416@gmail.com>
2021-03-12 11:46:57 -0500

    start working at routes

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ const app = express();
 const PORT = config.get("PORT") || 3001;
 
 const start = async () => {
-    try {
-      await mongoose.connect(config.get("mongoUri"), {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-      });
-      app.listen(PORT, () =>
-        console.log(`Server is listening PORT ${PORT}.......`)
-      );
-    } catch (e) {
-      console.log("Server Error", e.message);
-      process.exit(1);
-    }
-  };
-  
-  start();
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    app.listen(PORT, () =>
+      console.log(`Server is listening PORT ${PORT}.......`)
+    );
+  } catch (e) {
+    console.log("Server Error", e.message);
+    process.exit(1);
+  }
+};
+
+start();
