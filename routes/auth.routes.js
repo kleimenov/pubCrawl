4f1177c291c96ada9f6b84c80fe8a1dfe32e2da9
Иван <kleimenov416@gmail.com>
2021-03-12 11:56:11 -0500

    add two routes inside auth.route.js file, it is login and register routes

---
+++ routes/auth.routes.js
@@ -0,0 +1,17 @@
+const { Roruter, Router } = require('express');
+const router = Router();
+
+// /api/auth (all next routs will be concatinate with this path)
+
+
+//first auth route is:  /api/auth/register
+router.post('/register', async (req, res) => {
+
+})
+
+//second auth route is:  /api/auth/login
+router.post('/login', async (req, res) => {
+
+})
+
+module.exports = router
