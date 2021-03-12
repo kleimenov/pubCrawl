cd553dd80f566682c80a7aaa95c92ff84599d31e
Иван <kleimenov416@gmail.com>
2021-03-12 11:19:49 -0500

    add start function, it helps me work with db, I will work with promises and async funk helps me with this

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const app = express();
 
 const PORT = config.get("PORT") || 3001;
 
+const start = async () => {
+
+}
+
+start()
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}.....`);
 });
