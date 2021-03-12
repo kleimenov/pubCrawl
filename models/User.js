d73be293080af4b78a1d8fc5b74f6eed818ac77d
Иван <kleimenov416@gmail.com>
2021-03-12 12:29:50 -0500

    statrt working at reqistration logic

--- models/User.js
+++ models/User.js
@@ -1,1 +1,13 @@
-const {Schema, model} = require('mongoose')
+const { Schema, model, Types } = require("mongoose");
+
+const schema = new Schema({
+  name: { type: String, require: true },
+  email: {type: String, require: true, unique: true},
+  password: {type: String, require: true},
+  telephone_number: {type: String},
+  barsList: [ {type: Types.ObjectId, ref: 'Bars'}],
+  date: {type: Date, default: Date.now}
+
+});
+
+module.exports = model("User", schema);
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,17 +1,19 @@
-const { Roruter, Router } = require('express');
+const { Roruter, Router } = require("express");
+const User = require("../models/User");
 const router = Router();
 
 // /api/auth (all next routs will be concatinate with this path)
 
+//console.log(User.schema.obj)
 
 //first auth route is:  /api/auth/register
-router.post('/register', async (req, res) => {
-     
-})
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, telephone_number } = req.body;
+  } catch (e) {}
+});
 
 //second auth route is:  /api/auth/login
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {});
 
-})
-
-module.exports = router
+module.exports = router;
