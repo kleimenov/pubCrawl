39911ed1077152bd37437a9f9a8957945b7f0b63
Иван <kleimenov416@gmail.com>
2021-03-15 09:46:36 -0400

    work  at useHttp hook

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -4,6 +4,7 @@ import {useState} from 'react';
 
 export const useHttp = () => {
     const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
     const request = () => {}
 
