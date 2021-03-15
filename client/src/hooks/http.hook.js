693d546c9fefcb9650772a76522be930c206e761
Иван <kleimenov416@gmail.com>
2021-03-15 10:17:29 -0400

    start testing useHttp hook

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -10,6 +10,7 @@ export const useHttp = () => {
       try {
         if (body) {
           body = JSON.stringify(body);
+          headers["Content-Type"] = "application/json";
         }
         const response = await fetch(url, { method, body, headers });
         const data = response.json();
