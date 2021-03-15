39a638de2d16f21747d71c94d80b47b3cb8a1bfd
Иван <kleimenov416@gmail.com>
2021-03-15 09:51:13 -0400

    work  at useHttp hook

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -1,15 +1,14 @@
-import {useState, useCallback} from 'react';
-
-
+import { useState, useCallback } from "react";
 
 export const useHttp = () => {
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const request = useCallback(
-        async (url, method="GET", body= null, headers= {}) => {}
-        ) 
-        
+  const request = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      await fetch(url, { method, body, headers });
+    }
+  );
 
-    return { loading, request }
-}
+  return { loading, request };
+};
