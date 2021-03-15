b349443f76b7e9012fc2cefd80cfe427a067e6bd
Иван <kleimenov416@gmail.com>
2021-03-15 09:28:46 -0400

    start working on login page, add specific state

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AuthPage = () => {
+
+  const [form, setForm] = useState({
+    email: '', password: ''
+  })
+  
+
   return (
     <div>
       <div>
