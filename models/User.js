65f8ec4676cd628dbdae4ea247567d6e90b54a63
Иван <kleimenov416@gmail.com>
2021-03-16 17:07:46 -0400

    remove unnecessary lines of code

--- models/User.js
+++ models/User.js
@@ -2,12 +2,11 @@ const { Schema, model, Types } = require("mongoose");
 
 const schema = new Schema({
   name: { type: String, require: true },
-  email: {type: String, require: true, unique: true},
-  password: {type: String, require: true},
-  phone_number: {type: String},
-  barsList: [ {type: Types.ObjectId, ref: 'Bars'}],
-  date: {type: Date, default: Date.now}
-
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  phone_number: { type: String },
+  barsList: [{ type: Types.ObjectId, ref: "Bars" }],
+  date: { type: Date, default: Date.now },
 });
 /*
 const schema = new Schema({
