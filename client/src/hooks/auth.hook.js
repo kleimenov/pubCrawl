53dd9ca5d462168c3e637d7cd2d8e010741b76a9
Иван <kleimenov416@gmail.com>
2021-03-16 17:07:19 -0400

    remove unnecessary lines of code

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -4,7 +4,7 @@ const storageName = "userData";
 
 export const useAuth = () => {
   const [token, setToken] = useState(null);
-  const [ready, setReady] = useState(false)
+  const [ready, setReady] = useState(false);
   const [userId, setUserId] = useState(null);
 
   const login = useCallback((jwtToken, id) => {
@@ -45,7 +45,7 @@ export const useAuth = () => {
 
     if (data && data.token) {
       login(data.token, data.userId);
-      register(data.token, data.userId)
+      register(data.token, data.userId);
     }
     setReady(true);
   }, [login, register]);
