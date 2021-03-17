0eb70608789be6002f349b75863cdf00c73810c5
Иван <kleimenov416@gmail.com>
2021-03-17 10:36:20 -0400

    start working at styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,29 +14,29 @@ export const Navbar = () => {
 
   return (
     <nav>
-      <div className="bg-primary">
+      <div className="header-custom">
         <div className="d-flex flex-row justify-content-between px-5 pt-2">
-          <a className="text-white" href="/">
+          <a className="text-secondary" href="/">
             pubCrawl
           </a>
           <ul className="d-flex flex-row align-items-center list-unstyled">
           <li className="mx-2">
-              <NavLink className="text-white" to="/search">
+              <NavLink className="text-secondary h6" to="/search">
                 Search
               </NavLink>
             </li>
             <li className="mx-2">
-              <NavLink className="text-white" to="/create">
+              <NavLink className="text-secondary h6" to="/create">
                 Create
               </NavLink>
             </li>
             <li className="mx-2">
-              <NavLink className="text-white" to="/barslist">
+              <NavLink className="text-secondary h6" to="/barslist">
                 Bars list
               </NavLink>
             </li>
             <li className="mx-2">
-              <a onClick={logoutHandler} className="text-white" href="/">
+              <a onClick={logoutHandler} className="text-secondary h5" href="/">
                 logout
               </a>
             </li>
