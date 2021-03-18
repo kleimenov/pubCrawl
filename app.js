118003c95570e8acb1981d0de49f6aa79341d7fb
Иван <kleimenov416@gmail.com>
2021-03-18 17:30:34 -0400

    remove unnecessary lines of code

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const app = express();
 
 app.use(express.json({ extended: true }));
 
+//middleware
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/search", require("./routes/search.routes"));
+
 
 const PORT = config.get("PORT") || 3001;
 
