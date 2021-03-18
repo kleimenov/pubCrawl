eb5e88846092a9f16c8a98394c53effa6b38aeaa
Иван <kleimenov416@gmail.com>
2021-03-18 11:13:30 -0400

    continue working at search bars and particular model

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const app = express();
 app.use(express.json({ extended: true }));
 
 app.use("/api/auth", require("./routes/auth.routes"));
-app.use("/api/search", require(""))
-
+app.use("/api/search", require("./routes/search.routes"));
 
 const PORT = config.get("PORT") || 3001;
 
