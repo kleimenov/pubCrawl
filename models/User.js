74f1dfd638c8b500ef69d0eca70d805a7b6f81e8
Иван <kleimenov416@gmail.com>
2021-03-18 16:31:25 -0400

    implement Bars models

--- models/User.js
+++ models/User.js
@@ -8,11 +8,5 @@ const schema = new Schema({
   barsList: [{ type: Types.ObjectId, ref: "Bars" }],
   date: { type: Date, default: Date.now },
 });
-/*
-const schema = new Schema({
-  email: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  links: [{type: Types.ObjectId, ref: 'Link'}]
-})
-*/
+
 module.exports = model("User", schema);
