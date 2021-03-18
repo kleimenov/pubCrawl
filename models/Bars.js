d6b06fdf6af3beb02357c2aee477c25e7d32ca49
Иван <kleimenov416@gmail.com>
2021-03-18 16:15:28 -0400

    implement Bars models

--- models/Bars.js
+++ models/Bars.js
@@ -1,7 +1,14 @@
 const {Schema, model, Types} = require('mongoose');
 
 const schema = new Schema({
-    name: {type: String}
+    name: {type: String},
+    address: {type: String},
+    coordinate: {type: Object},
+    type: {type: String},
+    operation_hours: {type: Object},
+    website: {type: String},
+    phone: {type: String}
+
   })
 
 module.exports = model("Bars", schema)
