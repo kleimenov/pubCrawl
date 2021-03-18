9bcfdca1957ef250507a5642aaaa9601bf97a17f
Иван <kleimenov416@gmail.com>
2021-03-18 16:54:38 -0400

    filling db

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -30,6 +30,11 @@ export const SearchPage = () => {
   return (
     <div>
       <h3>SearchPage</h3>
+      {links.map((item, i) => (
+        <li>
+          {item.name}/ {item.type}
+        </li>
+      ))}
     </div>
   );
 };
