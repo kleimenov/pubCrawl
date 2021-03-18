32132050b1d262414aba887100769399e44ee415
Иван <kleimenov416@gmail.com>
2021-03-18 11:07:30 -0400

    working at model Bars

--- models/Bars.js
+++ models/Bars.js
@@ -1,1 +1,8 @@
 const {Schema, model, Types} = require('mongoose');
+
+const schema = new Schema({
+    barName: {type: String},
+    adress: {type: String},
+    coordinates: {type: Types.ObjectId},
+    hours: {type: Types.ObjectId},
+})
