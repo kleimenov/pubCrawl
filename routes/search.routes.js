b24d7acf0e5d1d3e60cc81a9826e9bd63f91e09d
Иван <kleimenov416@gmail.com>
2021-03-18 17:10:41 -0400

    remove unnecessary lines of code

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -1,18 +1,16 @@
-const {Router} = require('express');
-const Bars = require('../models/Bars');
+const { Router } = require("express");
+const Bars = require("../models/Bars");
 const router = Router();
 
 router.get("/", async (req, res) => {
-    //console.log("Xyi from server")
-    try {
-        const bars = await Bars.find({})
-        //console.log(bars)
-        res.json(bars);
-    } catch (e) {
-        res.status(500).json({ message: "Something goes wrong!" });
-    }
-})
+  //console.log("Xyi from server")
+  try {
+    const bars = await Bars.find({});
+    //console.log(bars)
+    res.json(bars);
+  } catch (e) {
+    res.status(500).json({ message: "Something goes wrong!" });
+  }
+});
 
-
-
-module.exports = router
+module.exports = router;
