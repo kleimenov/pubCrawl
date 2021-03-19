d8b2b375bad9c4f565c4d3846172a7ecd3d4167e
Иван <kleimenov416@gmail.com>
2021-03-19 09:54:36 -0400

    add couple of style settings

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -31,10 +31,74 @@ export const SearchPage = () => {
   return (
     <div>
       <h3>SearchPage</h3>
+      <Card className="text-left">
+        <Card.Body>
+          <Form className="text-secondary search-form-settings">
 
+          </Form>
+        </Card.Body>
+      </Card>
     </div>
   );
 }; 
+
+/*
+
+return (
+    <div>
+      <h3>SearchPage</h3>
+
+    </div>
+  );
+
+return (
+    <Card className="text-left">
+      <Card.Body>
+        <Form
+          className="text-secondary search-form-settings"
+          onSubmit={searchHandler}
+        >
+          <Form.Group id="Author">
+            <Form.Label>Search by Author</Form.Label>
+            <Form.Control
+              type="Author"
+              onChange={(evt) => setAuthor(evt.target.value)}
+            />
+          </Form.Group>
+          <Form.Group id="Genre">
+            <Form.Label>Search by Genre</Form.Label>
+            <Form.Control
+              type="Genre"
+              onChange={(evt) => setGenre(evt.target.value)}
+            />
+          </Form.Group>
+          <Form.Group id="Country">
+            <Form.Label>Search by Country</Form.Label>
+            <Form.Control
+              type="Country"
+              onChange={(evt) => setCountry(evt.target.value)}
+            />
+          </Form.Group>
+          <Form.Group id="bookName">
+            <Form.Label>Search by book's name</Form.Label>
+            <Form.Control
+              type="bookName"
+              onChange={(evt) => setBookName(evt.target.value)}
+            />
+          </Form.Group>
+          <Button
+            className="w-50 btn-secondary search-button-settings"
+            type="submit"
+          >
+            Search
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+
+  */
+
 
 /*
 export const SearchPage = () => {
--- routes/search.routes.js
+++ routes/search.routes.js
@@ -2,6 +2,19 @@ const { Router } = require("express");
 const Bars = require("../models/Bars");
 const router = Router();
 
+
+//get data from search engine, and post it on server and get result (specific query)
+router.post('/', async (req, res) => {
+  console.log('Xyi xyi')
+  const {} = req.body;
+
+  try {
+    res.json('Xyi')
+  } catch (e) {
+    res.status(500).json({ message: "Something goes wrong!" });
+  }
+})
+
 //get all data from Bars colllection. GET Route is:  /api/search/
 router.get("/", async (req, res) => {
   try {
@@ -12,15 +25,6 @@ router.get("/", async (req, res) => {
   }
 });
 
-route.post('/', async (req, res) => {
 
-  const {} = req.body;
-
-  try {
-    res.json('Xyi')
-  } catch (e) {
-    res.status(500).json({ message: "Something goes wrong!" });
-  }
-})
 
 module.exports = router;
