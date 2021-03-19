6b423f6f6373b2b927b50cb439c5fddffcb40b26
Иван <kleimenov416@gmail.com>
2021-03-19 09:03:31 -0400

    start working at search engine

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -36,16 +36,35 @@ export const SearchPage = () => {
   );
 }; 
 
-//for test
 /*
- return (
+export const SearchPage = () => {
+  const [links, setLinks] = useState([]);
+  const { loading, request } = useHttp();
+  const { token } = useContext(AuthContext);
+
+  const fetchLinks = useCallback(async () => {
+    try {
+      const fetched = await request("/api/search", "GET", null, {
+        Authorization: `Bearer ${token}`,
+      });
+      setLinks(fetched);
+    } catch (e) {}
+  }, [token, request]);
+
+  useEffect(() => {
+    fetchLinks();
+  }, [fetchLinks]);
+
+  if (loading) {
+    return <Loader />;
+  }
+  console.log(links);
+
+  return (
     <div>
       <h3>SearchPage</h3>
-      {links.map((item, i) => (
-        <li>
-          {item.name}/ {item.type}
-        </li>
-      ))}
+
     </div>
   );
-  */
+}; 
+*/
