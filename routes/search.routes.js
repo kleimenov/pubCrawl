1342054ffa39a4cb8c835ef7ad0233f4b3112a1e
Иван <kleimenov416@gmail.com>
2021-03-19 09:20:12 -0400

    working at search engine, create get route

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -2,10 +2,8 @@ const { Router } = require("express");
 const Bars = require("../models/Bars");
 const router = Router();
 
-
 //get all data from Bars colllection. GET Route is:  /api/search/
 router.get("/", async (req, res) => {
-  
   try {
     const bars = await Bars.find({});
     res.json(bars);
