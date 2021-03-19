d8643d2a02c882b317a02c3eab88902fcf38d007
Иван <kleimenov416@gmail.com>
2021-03-19 10:30:41 -0400

    start working at search route

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -3,20 +3,10 @@ const Bars = require("../models/Bars");
 const router = Router();
 
 //get data from search engine, and post it on server and get result (specific query)
-router.post("/", async (req, res) => {
-  console.log("Xyi xyi");
-  const {} = req.body;
-
-  try {
-    res.json("Xyi");
-  } catch (e) {
-    res.status(500).json({ message: "Something goes wrong!" });
-  }
-});
-
-//get all data from Bars colllection. GET Route is:  /api/search/
 router.get("/", async (req, res) => {
+  console.log("Xyi xyi");
   try {
+
     const bars = await Bars.find({});
     res.json(bars);
   } catch (e) {
@@ -24,4 +14,18 @@ router.get("/", async (req, res) => {
   }
 });
 
+
+//get all data from Bars colllection. GET Route is:  /api/search/
+/*
+router.get("/", async (req, res) => {
+  console.log("Xyi xyi");
+  try {
+
+    const bars = await Bars.find({});
+    res.json(bars);
+  } catch (e) {
+    res.status(500).json({ message: "Something goes wrong!" });
+  }
+});
+*/
 module.exports = router;
