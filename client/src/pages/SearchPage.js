f6ead804d34e7db7b2a87c6380bdc11481ba5338
Иван <kleimenov416@gmail.com>
2021-03-19 10:14:21 -0400

    add couple of style settings

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -70,51 +70,6 @@ return (
     </div>
   );
 
-return (
-    <Card className="text-left">
-      <Card.Body>
-        <Form
-          className="text-secondary search-form-settings"
-          onSubmit={searchHandler}
-        >
-          <Form.Group id="Author">
-            <Form.Label>Search by Author</Form.Label>
-            <Form.Control
-              type="Author"
-              onChange={(evt) => setAuthor(evt.target.value)}
-            />
-          </Form.Group>
-          <Form.Group id="Genre">
-            <Form.Label>Search by Genre</Form.Label>
-            <Form.Control
-              type="Genre"
-              onChange={(evt) => setGenre(evt.target.value)}
-            />
-          </Form.Group>
-          <Form.Group id="Country">
-            <Form.Label>Search by Country</Form.Label>
-            <Form.Control
-              type="Country"
-              onChange={(evt) => setCountry(evt.target.value)}
-            />
-          </Form.Group>
-          <Form.Group id="bookName">
-            <Form.Label>Search by book's name</Form.Label>
-            <Form.Control
-              type="bookName"
-              onChange={(evt) => setBookName(evt.target.value)}
-            />
-          </Form.Group>
-          <Button
-            className="w-50 btn-secondary search-button-settings"
-            type="submit"
-          >
-            Search
-          </Button>
-        </Form>
-      </Card.Body>
-    </Card>
-  );
 
   */
 
