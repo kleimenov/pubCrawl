091476cf8293ac1b87fedd092977c3269941992f
Иван <kleimenov416@gmail.com>
2021-03-22 16:30:15 -0400

    continue working on search engine logic, add some variables and error iside search.hook

--- client/src/hooks/search.hook.js
+++ client/src/hooks/search.hook.js
@@ -1,15 +1,18 @@
 import { useState, useCallback, useEffect } from "react";
 
-
 export const useSearch = () => {
-    const [search, setSearch] = useState(false)
-    const [ready, setReady] = useState(false);
-    const [response, setResponse] = useState([])
+  const [search, setSearch] = useState(false);
+  const [searching, setSearching] = useState(false);
+  const [error, setError] = useState(null);
 
-    const clearSearch = useCallback(() => {
-        setResponse([])
-        setSearch(false)
-    }, [])
+  const clearSearch = useCallback(() => {
+    try {
+      //throw new Error("Something went wrong")
+    } catch (e) {
+      //setError(e.message)
+      throw e;
+    }
+  }, []);
 
-    return {}
-}
+  return {search, searching, error};
+};
