953e8a806797e8186d494045eecdabdb88a592e0
Иван <kleimenov416@gmail.com>
2021-03-22 18:16:35 -0400

    remove unnecessary lines of code

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useContext, useEffect, useState } from "react";
 import { useSearch } from "../hooks/search.hook";
-//import { SearchContext } from "../context/SearchContext";
+import { SearchContext } from "../context/SearchContext";
 import { SearchEngine } from "../components/SearchEngine";
 import { SearchResult } from "../components/SearchResult";
 import { Loader } from "../components/Loader";
