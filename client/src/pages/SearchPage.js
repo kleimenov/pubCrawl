f524ca4fd2302a293a6b9078a4bbb9610710f637
Иван <kleimenov416@gmail.com>
2021-03-25 16:04:14 -0400

    remove unnecessary lines of code

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,7 +1,6 @@
 import React, { useCallback, useContext, useEffect, useState } from "react";
 import { SearchEngine } from "../components/SearchEngine";
 
-
 export const SearchPage = () => {
-  return <SearchEngine />
+  return <SearchEngine />;
 };
