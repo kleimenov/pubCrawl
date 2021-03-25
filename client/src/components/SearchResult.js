c76d6c9903f12738ce8554455fa1bd1ae24e171f
Иван <kleimenov416@gmail.com>
2021-03-25 11:40:29 -0400

    continue working at search logic

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -1,31 +1,25 @@
 import React, { useState } from "react";
 import { useHttp } from "../hooks/http.hook";
 
+export const SearchResult = ({ data, isSearched }) => {
+  const { loading } = useHttp();
+  console.log(data);
 
-
-export const SearchResult = ({data, isSearched}) => {
-  const {loading} = useHttp()
-  console.log(data)
-
-  
-  const searchHandler = () => {
-    try {
-     isSearched()
-    } catch (e) {
-    }
-  }
+  const returnHandler = () => {
+    isSearched();
+  };
   return (
     <div>
-        Hello you are on SearchResult component!
-        <div className="d-flex justify-content-between align-items-center w-75 mt-3">
-          <button
-            className="btn btn-secondary text-white py-1 px-2"
-            onClick={searchHandler}
-            disabled={loading}
-          >
-            Back to Search
-          </button>
-          </div>
+      Hello you are on SearchResult component!
+      <div className="d-flex justify-content-between align-items-center w-75 mt-3">
+        <button
+          className="btn btn-secondary text-white py-1 px-2"
+          onClick={returnHandler}
+          disabled={loading}
+        >
+          Back to Search
+        </button>
+      </div>
     </div>
-    );
+  );
 };
