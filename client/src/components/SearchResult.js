af4daa64bb9ed67d74ebe427ca94442f35d3f9ad
Иван <kleimenov416@gmail.com>
2021-03-25 12:19:26 -0400

    implement search result logic, get fetched data from server and map this response on client side

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { useHttp } from "../hooks/http.hook";
+import { Button } from "react-bootstrap";
 
 export const SearchResult = ({ data, isSearched }) => {
   const { loading } = useHttp();
@@ -10,8 +11,28 @@ export const SearchResult = ({ data, isSearched }) => {
   };
   return (
     <div>
-      <h4 className="text-secondary text-right">This is result of your search:</h4> 
-      <div className="d-flex justify-content-between align-items-center w-75 mt-3">
+      <h4 className="text-secondary text-right">
+        This is result of your search:
+      </h4>
+      <ul>
+        {data.map((item) => (
+          <li className="my-2">
+            <div className="search-wrapper">
+              <h5 className="text-secondary ml-2 pt-2">
+                {item.name} / {item.type} / {item.district}
+              </h5>
+              <Button
+                className="btn-primary p-0 px-1 py-0 ml-1"
+                type="submit"
+              >
+                +bar
+              </Button>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      <div className="d-flex justify-content-end align-items-center mt-5">
         <button
           className="btn btn-secondary text-white py-1 px-2"
           onClick={returnHandler}
