c87c185b9d84267fac919b2470536da4bdd34579
Иван <kleimenov416@gmail.com>
2021-03-26 15:21:21 -0400

    remove unnecessary lines of code

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -1,20 +1,14 @@
 const { Router } = require("express");
 const Bars = require("../models/Bars");
-const toCapitalize = require('../handlers/functions.handlers')
+const toCapitalize = require("../handlers/functions.handlers");
 const router = Router();
 
 //get data from search engine, and post it on server and get result (specific query)
 router.post("/", async (req, res) => {
-
-  
-
   //get data from client side
   const data = req.body;
-  console.log(data)
+  console.log(data);
   //check what data we have
-  
-  
-  
 
   try {
     const bars = await Bars.find({ address: { $regex: "M6" } });
