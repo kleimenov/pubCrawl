e52369e1d45cf7f8c73b09664bd153453f6567fe
Иван <kleimenov416@gmail.com>
2021-03-26 09:52:44 -0400

    add function toCapitalize, I will use this function for transform data that come form db

---
+++ client/src/handlers/functions.handlers.js
@@ -0,0 +1,13 @@
+const toCapitalize = (string) => {
+    let splitStr = string.split(" ");
+    let newArr = [];
+    for (let word of splitStr) {
+      if (word.length > 1) {
+        word = word.charAt(0).toUpperCase() + word.slice(1);
+        newArr.push(word);
+      } else {
+        newArr.push(word);
+      }
+    }
+    return newArr.join(" ");
+  };
