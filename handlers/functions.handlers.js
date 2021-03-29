a3d547d971acd5d87dce626444115bc93b4eca36
Иван <kleimenov416@gmail.com>
2021-03-29 10:02:45 -0400

    implement toLowercase() function

--- handlers/functions.handlers.js
+++ handlers/functions.handlers.js
@@ -1,5 +1,3 @@
-
-
 const toCapitalize = (string) => {
   let splitStr = string.split(" ");
   let newArr = [];
@@ -14,9 +12,24 @@ const toCapitalize = (string) => {
   return newArr.join(" ");
 };
 
-  module.exports = {
-    toCapitalize
+const toLowercase = () => {
+  let splitStr = string.split(" ");
+  let newArr = [];
+  for (let word of splitStr) {
+    if (word.length > 1) {
+      word = word.charAt(0).toLowerCase() + word.slice(1);
+      newArr.push(word);
+    } else {
+      newArr.push(word);
+    }
   }
+  return newArr.join(" ");
+};
+
+module.exports = {
+  toCapitalize,
+  toLowercase
+};
 
 /*
 export const handlersFunctions = () => {
