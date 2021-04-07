3a19c7d0bc46b2ebefca33047ced8a2ef42d4f04
Иван <kleimenov416@gmail.com>
2021-04-07 10:22:52 -0400

    clean code, refactor some lines and remove unnecessary lines

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -14,7 +14,6 @@ router.post("/", async (req, res) => {
   try {
     //const bars = await Bars.find({ address: { $regex: "M6" } }); //reg expression just in case
     const bars = await Bars.find(data);
-    
     res.json(bars);
   } catch (e) {
     res.status(500).json({ message: "Something goes wrong!" });
