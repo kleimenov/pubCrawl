c5695d10d1efb4045445e87d6c18f647be80b880
Иван <kleimenov416@gmail.com>
2021-04-08 09:20:57 -0400

    clean code, remove some lines of code, that I won't  use anymore

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -8,7 +8,8 @@ router.post("/", async (req, res) => {
   //get data from client side
   const data = req.body;
   
-  //apply validation function, transform data to lowercase, currently I won't use this function, anyway I will store it here just in case.
+  //apply validation function, transform data to lowercase, currently I won't use this function,
+  //anyway I will store it here just in case.
   const validatedData = handlers.queryValidator(data);
   
 
