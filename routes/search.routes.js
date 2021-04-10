bd6e294fb52a9ae385a8a3c1ebe5236eba854366
Иван <kleimenov416@gmail.com>
2021-04-10 10:27:09 -0400

    work at search engine, modify query

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -15,7 +15,7 @@ router.post("/", async (req, res) => {
 
   try {
     //const bars = await Bars.find({ address: { $regex: "M6" } }); //reg expression just in case
-    const bars = await Bars.find({$or: [{"barName":data.barName}, {"type":data.type}, {"district":data.district}]});
+    //const bars = await Bars.find({$or: [{"barName":data.barName}, {"type":data.type}, {"district":data.district}]});
     //const bars = await Bars.find({$or: [{barName:validatedData.barName}, {type:validatedData.type}, {district:validatedData.district}]});
     
     
