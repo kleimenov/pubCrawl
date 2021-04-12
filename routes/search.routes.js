24af57a27157da3d90781e0f54912ce81b7b6409
Иван <kleimenov416@gmail.com>
2021-04-12 11:15:24 -0400

    implemented multi words query, used regular expression under the hood, and also implement check algorithm, if bar name hasn't set, we will search just for other parameters

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -11,30 +11,36 @@ router.post("/", async (req, res) => {
   //apply validation function, transform data to lowercase, currently I won't use this function,
   //anyway I will store it here just in case.
   const validatedData = handlers.queryValidator(data);
-  let regex = new RegExp(validatedData.barName, 'i');
-  console.log(regex)
-  console.log(validatedData.barName)
+  let regex = new RegExp(validatedData.barName, "i");
+  console.log(regex);
+  console.log(validatedData.barName);
 
-  try {
-    //const bars = await Bars.find({ address: { $regex: "M6" } }); //reg expression just in case
-    //const bars = await Bars.find({$or: [{"barName":data.barName}, {"type":data.type}, {"district":data.district}]});
-    const bars = await Bars.find({
-      $or: [
+  if (validatedData.barName) {
+    try {
+      //const bars = await Bars.find({ address: { $regex: "M6" } }); //reg expression just in case
+      //const bars = await Bars.find({$or: [{"barName":data.barName}, {"type":data.type}, {"district":data.district}]});
+      const bars = await Bars.find({
+        $or: [
           //{ barName: `/^${validatedData.type}$/i`},
-          { barName: regex},
-        //{ barName: validatedData.barName },
 
-        { type: validatedData.type },
-        { district: validatedData.district },
-      ],
-    });
-
-    //console.log('data come from server', bars)
-
-    res.json(bars);
-  } catch (e) {
-    res.status(500).json({ message: "Something goes wrong!" });
+          //{ barName: validatedData.barName },
+          { barName: regex },
+          { type: validatedData.type },
+          { district: validatedData.district },
+        ],
+      });
+      //console.log('data come from server', bars)
+      res.json(bars);
+    } catch (e) {
+      res.status(500).json({ message: "Something goes wrong!" });
+    }
   }
+  try {
+    const bars = await Bars.find({
+      $or: [{ type: validatedData.type }, { district: validatedData.district }],
+    });
+    res.json(bars);
+  } catch (e) {}
 });
 
 //get all data from server (query select all)
