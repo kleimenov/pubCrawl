3ae614c4b24b7726a10bec020a1b811906e0e694
Иван <kleimenov416@gmail.com>
2021-04-12 11:19:50 -0400

    remove unnecessary lines of code, and make code more readable

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -8,6 +8,7 @@ router.post("/", async (req, res) => {
   //check data that came from client side
   const data = req.body;
   console.log(data);
+  
   //apply validation function, transform data to lowercase, currently I won't use this function,
   //anyway I will store it here just in case.
   const validatedData = handlers.queryValidator(data);
