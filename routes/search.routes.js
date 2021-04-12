135cca0245223c365fbbb0c6a25c9d19a34d4845
Иван <kleimenov416@gmail.com>
2021-04-12 11:02:06 -0400

    continue working at multi-words queries, implement regEx  to check is there part of word or whole word in query

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -11,13 +11,18 @@ router.post("/", async (req, res) => {
   //apply validation function, transform data to lowercase, currently I won't use this function,
   //anyway I will store it here just in case.
   const validatedData = handlers.queryValidator(data);
+  let regex = new RegExp(validatedData.barName, 'i');
+  console.log(regex)
+  console.log(validatedData.barName)
 
   try {
     //const bars = await Bars.find({ address: { $regex: "M6" } }); //reg expression just in case
     //const bars = await Bars.find({$or: [{"barName":data.barName}, {"type":data.type}, {"district":data.district}]});
     const bars = await Bars.find({
       $or: [
-        { barName: validatedData.barName },
+          //{ barName: `/^${validatedData.type}$/i`},
+          { barName: regex},
+        //{ barName: validatedData.barName },
 
         { type: validatedData.type },
         { district: validatedData.district },
