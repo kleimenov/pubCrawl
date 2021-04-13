1887acff096d6f099e796004548f416e7e2637b7
Иван <kleimenov416@gmail.com>
2021-04-13 12:26:22 -0400

    add new middleware /api/barslist inside app.js

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const app = express();
 
 //app.use(cors());
 
-app.use(express.json({ extended: true }));
+app.use(express.json({ extended: true })); 
 
 //middleware
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/search", require("./routes/search.routes"));
+app.use("/api/barslist", require("./routes/barslist.routes"));
 
 //set port settings
 const PORT = config.get("PORT") || 3001;
