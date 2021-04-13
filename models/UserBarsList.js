1cbff6a7c52a12b469534bca2fa49575c8917d62
Иван <kleimenov416@gmail.com>
2021-04-13 12:35:49 -0400

    continue working at adding bar into user's bars list

--- models/UserBarsList.js
+++ models/UserBarsList.js
@@ -2,6 +2,8 @@ const { Schema, model, Types } = require("mongoose");
 
 
 //schema is under construction
-const schema = new Schema({})
+const schema = new Schema({
+    
+})
 
 module.exports = model("UserBarsList", schema);
