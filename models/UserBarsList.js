3aa8bb6a42686c59b19e77b9e1d8a2536e9b988f
Ivan <kleimenov416@gmail.com>
2021-04-14 19:57:01 -0400

    create UserBarsList model and add inside data types and names

--- models/UserBarsList.js
+++ models/UserBarsList.js
@@ -3,7 +3,13 @@ const { Schema, model, Types } = require("mongoose");
 
 //schema is under construction
 const schema = new Schema({
-    
+    barName: {type: String},
+    address: {type: String},
+    coordinate: {type: Object},
+    type: {type: String},
+    operation_hours: {type: Object},
+    website: {type: String},
+    phone: {type: String}
 })
 
 module.exports = model("UserBarsList", schema);
