ccbb89e8c681fef1eeb6f9b881aa1bb0b9649c97
Ivan <kleimenov416@gmail.com>
2021-04-15 17:20:18 -0400

    implement user bars route, currently I can get list of user bars from User model, and after get bars data from Bars model and send it as response to clien side

--- routes/barslist.routes.js
+++ routes/barslist.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const User = require("../models/User");
+const Bars = require("../models/Bars")
 const router = Router();
 
 /*
@@ -11,7 +12,16 @@ router.post("/userbars", async (req, res) => {
   const userBarsData = []
   try {
     const userBars = await User.find(data);
-    console.log(userBars[0].barsList);
+    //console.log(userBars[0].barsList);
+    for (let item of userBars[0].barsList) {
+      const barId = { _id: item }
+      //const barId = { _id: `${item}`}
+      const bar = await Bars.find(barId);
+      //console.log(bar[0])
+      userBarsData.push(bar[0])
+      //userBarsData = [...userBarsData, bar]
+    }
+    console.log(userBarsData)
     res.json(userBars);
   } catch (e) {
     res.status(500).json({ message: "Something goes wrong!" });
