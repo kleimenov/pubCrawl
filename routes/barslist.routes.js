4512675c2d7b035caf92658d9def7c73707d21da
Ivan <kleimenov416@gmail.com>
2021-04-15 17:04:22 -0400

    add userBarsData, it is a variable where I will store specific user bars data

--- routes/barslist.routes.js
+++ routes/barslist.routes.js
@@ -4,17 +4,18 @@ const router = Router();
 
 /*
 all bars list logic will be here
-*/ 
+*/
 
 router.post("/userbars", async (req, res) => {
-    const data = {_id: req.body.userId}
-    try {
-      const userBars = await User.find({_id: '6050cd4f4790ac71d8f3e818'});
-      console.log(userBars[0].barsList)
-      res.json(userBars);
-    } catch (e) {
-      res.status(500).json({ message: "Something goes wrong!" });
-    }
-  });
+  const data = { _id: req.body.userId };
+  const userBarsData = []
+  try {
+    const userBars = await User.find(data);
+    console.log(userBars[0].barsList);
+    res.json(userBars);
+  } catch (e) {
+    res.status(500).json({ message: "Something goes wrong!" });
+  }
+});
 
 module.exports = router;
