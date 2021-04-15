3475d762c77484c0299cb214ab2c754e8765cfe0
Ivan <kleimenov416@gmail.com>
2021-04-15 11:23:45 -0400

    continue working at user bars route

--- routes/barslist.routes.js
+++ routes/barslist.routes.js
@@ -7,8 +7,8 @@ all bars list logic will be here
 */
 router.post("/barslist", async (req, res) => {
   try {
-      
-
+    const userBars = await Bars.find({});
+    res.json(userBars);
   } catch (e) {
     res.status(500).json({ message: "Something goes wrong!" });
   }
