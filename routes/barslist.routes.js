e7b7bd410c7d33eb0bf2f1e247bb556d4b8e0107
Ivan <kleimenov416@gmail.com>
2021-04-15 17:22:01 -0400

    remove unnecessary lines of code

--- routes/barslist.routes.js
+++ routes/barslist.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const User = require("../models/User");
-const Bars = require("../models/Bars")
+const Bars = require("../models/Bars");
 const router = Router();
 
 /*
@@ -9,17 +9,17 @@ all bars list logic will be here
 
 router.post("/userbars", async (req, res) => {
   const data = { _id: req.body.userId };
-  const userBarsData = []
+  const userBarsData = [];
   try {
     const userBars = await User.find(data);
     for (let item of userBars[0].barsList) {
-      const barId = { _id: item }
+      const barId = { _id: item };
       const bar = await Bars.find(barId);
-     
-      userBarsData.push(bar[0])
+
+      userBarsData.push(bar[0]);
       //userBarsData = [...userBarsData, bar]
     }
-    
+
     res.json(userBarsData);
   } catch (e) {
     res.status(500).json({ message: "Something goes wrong!" });
