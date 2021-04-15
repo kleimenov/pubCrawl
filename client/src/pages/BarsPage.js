0dae566f5ea4baf431c121900d0a22762d9aef09
Ivan <kleimenov416@gmail.com>
2021-04-15 11:38:43 -0400

    continue working at BarsPage

--- client/src/pages/BarsPage.js
+++ client/src/pages/BarsPage.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { useHttp } from "../hooks/http.hook";
-import {} from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 
 export const BarsPage = () => {
   const { loading, error, request } = useHttp();
