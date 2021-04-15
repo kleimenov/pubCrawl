f8ae55501eca9eb44b45387e7871c4649118eddd
Ivan <kleimenov416@gmail.com>
2021-04-15 11:33:17 -0400

    continue working at user bars route

--- client/src/components/BarsLisr.js
+++ client/src/components/BarsLisr.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { useHttp } from "../hooks/http.hook";
 
 export const Bar = () => {
-    
-}
+  const { loading, error, request } = useHttp();
+};
