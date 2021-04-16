e3a0951cc24f40eae4fc0ca84b25bed27404b1e8
Ivan <kleimenov416@gmail.com>
2021-04-16 16:24:00 -0400

    remove some unnecessary lines of code

--- client/src/pages/BarsPage.js
+++ client/src/pages/BarsPage.js
@@ -35,14 +35,11 @@ export const BarsPage = () => {
               {item.barName}
               <div>
                 <ul className="mt-2">
-                  {
-                    Object.keys(item.operation_hours).map((key) => (
-                      <li className="h6">
-                        {key} : {item.operation_hours[key]}
-                      </li>
-                    ))
-                    
-                  }
+                  {Object.keys(item.operation_hours).map((key) => (
+                    <li className="h6">
+                      {key} : {item.operation_hours[key]}
+                    </li>
+                  ))}
                 </ul>
               </div>
             </h5>
