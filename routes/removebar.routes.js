af536f2d7b7e1ba265f778c0d9d20cd51f843c29
Ivan <kleimenov416@gmail.com>
2021-04-22 11:47:21 -0400

    start working at remove bar middleware

--- routes/removebar.routes.js
+++ routes/removebar.routes.js
@@ -1,3 +1,12 @@
 const { Router } = require("express");
 const User = require("../models/User");
 const router = Router();
+
+router.post("/removebar", async (req, res) => {
+  console.log(req.body);
+  // try {
+
+  //   } catch (e) {
+  //     res.status(500).json({ message: "Something goes wrong!" });
+  //   }
+});
