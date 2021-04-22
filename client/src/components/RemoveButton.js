aff697d73b74802e86a12fe501a3722fbe676bde
Ivan <kleimenov416@gmail.com>
2021-04-22 11:14:50 -0400

    remove unnecessary lines of code

--- client/src/components/RemoveButton.js
+++ client/src/components/RemoveButton.js
@@ -1,14 +1,8 @@
 import React from "react";
 
 export const RemoveButton = () => {
-
-
-
-
   return (
-    <button
-      className="btn btn-secondary text-white px-1 mr-5"
-    >
+    <button className="btn btn-secondary text-white px-1 mr-5">
       Remove bar
     </button>
   );
