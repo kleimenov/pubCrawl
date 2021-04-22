27981d825984f2b30adc0e6f929615541fee4398
Ivan <kleimenov416@gmail.com>
2021-04-22 16:54:03 -0400

    continue working at remove book feature

--- routes/removebar.routes.js
+++ routes/removebar.routes.js
@@ -9,11 +9,27 @@ all bars list logic will be here
 router.post("/remove", async (req, res) => {
   console.log(req.body.removeId)
   console.log(req.body)
+
+  const filter = {_id: req.body.userId};
+  const deleteId = {barsList: req.body.removeId};
+  //console.log(filter, deleteId)
  
   //User.update({_id: user._id}, {$unset: {field: 1 }}, callback);
   try {
     //const testO = await User.deleteOne( { _id: req.body.userId }, { $pull: {barsList: req.body.removeId } } );
     //User.updateOne({ _id: req.body.userId }, { $pull: {barsList: [req.body.removeId] }})
+    // User.updateOne(
+    //     { _id: `${req.body.userId}` },
+    //     { $pull: { barsList: `${req.body.removeId}` } },
+    //     { multi: true }
+    // );
+    // const testTest = await User.find({ _id: req.body.userId }).then((response)=>{
+    //     if (response[0].barsList.includes(req.body.removeId)){
+    //          response.update({ $pull: {barsList: req.body.removeId } }) }
+        
+    // })
+    const testTest = await User.findById(filter)
+    console.log(testTest)
     res.json("love you");
   } catch (e) {
     res.status(500).json({ message: "Something goes wrong!" });
