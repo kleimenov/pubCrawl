6d8f7ab00a395867b55cde228eb62f2846e617a6
Ivan <kleimenov416@gmail.com>
2021-04-22 15:55:09 -0400

    continue working at remove book feature

--- routes/removebar.routes.js
+++ routes/removebar.routes.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 const User = require("../models/User");
-const Bars = require("../models/Bars");
 const router = Router();
 
 /*
@@ -10,9 +9,11 @@ all bars list logic will be here
 router.post("/remove", async (req, res) => {
   console.log(req.body.removeId)
   console.log(req.body)
-  
+ 
   //User.update({_id: user._id}, {$unset: {field: 1 }}, callback);
   try {
+    //const testO = await User.deleteOne( { _id: req.body.userId }, { $pull: {barsList: req.body.removeId } } );
+    //User.updateOne({ _id: req.body.userId }, { $pull: {barsList: [req.body.removeId] }})
     res.json("love you");
   } catch (e) {
     res.status(500).json({ message: "Something goes wrong!" });
