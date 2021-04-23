f345f5fbe93a7253c21309b40036086806c98f32
Ivan <kleimenov416@gmail.com>
2021-04-23 11:54:17 -0400

    remove some console.log

--- client/src/components/RemoveButton.js
+++ client/src/components/RemoveButton.js
@@ -3,7 +3,7 @@ import React from "react";
 export const RemoveButton = ({ barId, setRemove }) => {
   
   const clickHandler = () => {
-    console.log("button clicked", barId);
+    
     setRemove(barId);
   };
   return (
