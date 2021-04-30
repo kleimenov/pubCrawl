d7084c700d25308acddb5ee3caf537f90d11e5b2
Ivan <kleimenov416@gmail.com>
2021-04-30 17:04:50 -0400

    add some line of code

--- client/src/components/OperationHours.js
+++ client/src/components/OperationHours.js
@@ -1,12 +1,13 @@
 import React from "react";
 
+
 export const OperationHours = (item) => {
   return (
     <div>
       <ul className="mt-2">
         {Object.keys(item.item).map((key, index) => (
           <li className="h6 text-secondary" key={index}>
-            {key} : {item.item[key]}
+            {key} : {item.item[key]} 
           </li>
         ))}
       </ul>
