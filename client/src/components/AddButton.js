d84a5dd45717b6fb9d21a84e9f8f8ed0fcd73442
Ivan <kleimenov416@gmail.com>
2021-05-08 10:31:12 -0400

    add AddBotton function

--- client/src/components/AddButton.js
+++ client/src/components/AddButton.js
@@ -1,5 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export const AddButton = () => {
-    
-}
+  return (
+    <button
+      onClick={() => {
+        console.log("add button was clicked");
+      }}
+      className="btn btn-secondary text-white px-1 mr-5"
+    ></button>
+  );
+};
