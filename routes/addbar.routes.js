a0d72196a4afabac700326ef7b0104994d554c78
Ivan <kleimenov416@gmail.com>
2021-05-08 10:18:32 -0400

    add data variable

--- routes/addbar.routes.js
+++ routes/addbar.routes.js
@@ -3,10 +3,16 @@ const UserBarList = require("../models/UserBarsList");
 const route = Router();
 
 route.post("/add", async (req, res) => {
-    const data;
-    try {
-
-    } catch (e) {
-        res.status(500).json({ message: "Something goes wrong!" });
-    }
-})
+  const data;
+  try {
+    const barList = new UserBarList({
+      /*here I will add bar's data that I want to add in DB */
+    });
+    await barList.save();
+    res
+      .status(201)
+      .json({ message: "New was added", userId: user.id });
+  } catch (e) {
+    res.status(500).json({ message: "Something goes wrong!" });
+  }
+});
