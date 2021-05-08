24664e27771e4d5556bf6c58c1a5de582b7a96d9
Ivan <kleimenov416@gmail.com>
2021-05-08 10:16:08 -0400

    add data variable

--- routes/addbar.routes.js
+++ routes/addbar.routes.js
@@ -3,8 +3,9 @@ const UserBarList = require("../models/UserBarsList");
 const route = Router();
 
 route.post("/add", async (req, res) => {
+    const data;
     try {
-        
+
     } catch (e) {
         res.status(500).json({ message: "Something goes wrong!" });
     }
