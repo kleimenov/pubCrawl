79bf202e64b44d3a556c8a8f6a93390e644ab35c
Ivan <kleimenov416@gmail.com>
2021-05-08 10:10:43 -0400

    create add new bar into user bar list route structure

--- routes/addbar.routes.js
+++ routes/addbar.routes.js
@@ -2,3 +2,10 @@ const { Router } = require("express");
 const UserBarList = require("../models/UserBarsList");
 const route = Router();
 
+route.post("/add", async (req, res) => {
+    try {
+        
+    } catch (e) {
+        res.status(500).json({ message: "Something goes wrong!" });
+    }
+})
