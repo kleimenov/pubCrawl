4ebd610aa093a39b80f876dae1474757e856a402
Ivan <kleimenov416@gmail.com>
2021-05-08 10:19:48 -0400

    add respons object with message and onther data

--- routes/addbar.routes.js
+++ routes/addbar.routes.js
@@ -6,12 +6,14 @@ route.post("/add", async (req, res) => {
   const data;
   try {
     const barList = new UserBarList({
-      /*here I will add bar's data that I want to add in DB */
+      /*
+      here I will add bar's data that I want to add in DB 
+      */
     });
     await barList.save();
     res
       .status(201)
-      .json({ message: "New was added", userId: user.id });
+      .json({ message: "New was added", userId: user.id, barId: bar.id });
   } catch (e) {
     res.status(500).json({ message: "Something goes wrong!" });
   }
